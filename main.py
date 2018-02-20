0e0f7ca0e9e4bcb4d3e4b85a4c416145b09b197c
Andre Miras <andre.miras@gmail.com>
2018-02-20 22:01:52 +0100

    Toolbar and screen management
    Dynamically updates the toolbar to go back to the previous screen.

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ from kivy.clock import Clock
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
 from kivymd.theming import ThemeManager
+from kivymd.toolbar import Toolbar
+
+
+class CustomToolbar(Toolbar):
+
+    def __init__(self, **kwargs):
+        super(CustomToolbar, self).__init__(**kwargs)
+        Clock.schedule_once(self.load_default_buttons)
+
+    def load_default_buttons(self, dt=None):
+        self.left_action_items = [['menu', lambda x: None]]
+        self.right_action_items = [['dots-vertical', lambda x: None]]
+
+    def load_back_button(self, function):
+        # lambda x: app.root.toggle_nav_drawer()
+        self.left_action_items = [['arrow-left', lambda x: function()]]
 
 
 class QRScanScreen(Screen):
@@ -27,6 +43,7 @@ class QRScanScreen(Screen):
         # trigger `on_symbols`
         if not symbols:
             return
+        self.manager.transition.direction = 'left'
         self.manager.current = 'qrfound_screen'
         qrfound_screen = self.manager.current_screen
         symbol = symbols[0]
@@ -34,7 +51,26 @@ class QRScanScreen(Screen):
 
 
 class QRFoundScreen(Screen):
+
     data_property = StringProperty()
+
+    def on_back(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'qrscan_screen'
+
+    def on_enter(self):
+        """
+        Loads the toolbar back button.
+        """
+        app = App.get_running_app()
+        app.root.ids.toolbar_id.load_back_button(self.on_back)
+
+    def on_leave(self):
+        """
+        Loads the toolbar default button.
+        """
+        app = App.get_running_app()
+        app.root.ids.toolbar_id.load_default_buttons()
 
 
 class MainApp(App):
