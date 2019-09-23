e5c5fe062e5def401b7ab4c804313dde391a07c8
Andre Miras <andre.miras@gmail.com>
2019-09-24 01:09:51 +0200

    Updates version.py, refs #29

--- src/qrscan/version.py
+++ src/qrscan/version.py
@@ -1,1 +1,7 @@
 __version__ = '2019.0911'
+# The `__version_code__` is used for the F-Droid auto update and should match
+# the `versionCode` from:
+# `.buildozer/android/platform/build/dists/qrscan/build.gradle`
+# The auto update method used is the `HTTP`, see:
+# https://f-droid.org/en/docs/Build_Metadata_Reference/#UpdateCheckMode
+__version_code__ = 721202811
